5ae7aae298967ae03bf1d173af8bf1f2eea1eba1
eriksape <erik.sape@gmail.com>
2020-05-27 18:50:48 -0500

    [FIX]`categories endpoints`: added some things to the endpoints
    `id` to response in Categories models
    get news from categories endpoint

--- django-on-docker/app/categories/urls.py
+++ django-on-docker/app/categories/urls.py
@@ -9,4 +9,5 @@ router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("categories/<int:id>/news/", views.get_news, name="details"),
 ]
