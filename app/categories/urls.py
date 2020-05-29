ce9fc6da45ec6b2557dd6a21935bbde47d5774bb
eriksape <erik.sape@gmail.com>
2020-05-29 12:32:26 -0500

    [Feature]`categories routes`:<This enable categories routes>

--- app/categories/urls.py
+++ app/categories/urls.py
@@ -5,7 +5,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("categories/<int:id>/news/", views.get_news, name="get_news"),
 ]
+
