b9721264e400d55e0cc17850ab8758e1e04e45ea
eriksape <erik.sape@gmail.com>
2020-05-31 23:35:18 -0500

    [FEATURE]`sources`:<set id for endpoint>

--- app/sources/serializers.py
+++ app/sources/serializers.py
@@ -4,14 +4,17 @@ from rest_framework import serializers
 
 from .models import Source
 
+
 class SourceSerializer(serializers.ModelSerializer):
     """Set the fields to be use in the view"""
 
     class Meta:
         model = Source
-        fields = ('name',
-                  'url',
-                  'feed_url',
-                  'feed_type',
-                  'disclaimer',
-                  'logo')
+        fields = (
+            'id',
+            'name',
+            'url',
+            'feed_url',
+            'feed_type',
+            'disclaimer',
+            'logo')
